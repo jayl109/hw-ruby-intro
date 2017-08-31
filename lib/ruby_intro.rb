6490ae883d458acfda5062d178f09a21740a7cee
jason lum <jthemathfreak@gmail.com>
2017-08-31 22:10:47 +0000

    done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,105 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  $s = 0
+  $i = 0
+  while $i < arr.length do
+   $s= $s + arr.at($i)
+   $i+=1
+  end
+  return $s
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  $len = arr.length
+  if $len == 0
+    return 0
+  elsif $len ==1
+    return arr.at(0)
+  end
+  $s = arr.at(0) > arr.at(1) ? arr.at(0) : arr.at(1)
+  $s2 = arr.at(0) < arr.at(1) ? arr.at(0) : arr.at(1)
+  $i = 2
+  while $i < arr.length do
+   $cur = arr.at($i)
+   if $cur > $s 
+     $s2 = $s
+     $s = $cur
+   elsif $cur >$s2 
+     $s2 = $cur
+   end
+   $i+=1
+  end
+  return $s+$s2
 end
 
+ 
+
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  h = Hash.new("no")
+  arr.each do |x| 
+    if (h[n-x]!="no") 
+      return true
+    else
+      h[x]="yes"
+    end
+      
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, "+name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s[0] == nil
+    return false
+  end
+  $a = s[0].downcase
+  return $a =~ /[[:alpha:]]/ &&$a != "a" && $a != "e" && $a != "i" && $a != "o" && $a != "u"
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s.length == 0
+    return false
+  end
+  s.each_char do |x| 
+    if !(x == "1" || x == "0")
+      return false
+    end
+  end
+  
+  return s.to_i(2)%4 == 0
+  
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(s,n)
+    if (s == '' || n <= 0)
+      raise ArgumentError
+    end
+    @isbn = s
+    @price = n
+  end
+  def isbn
+    return @isbn 
+  end
+  def isbn=(isbn)
+    @isbn=isbn
+  end
+  def price
+    return @price 
+  end
+  def price=(price)
+    @price = price
+  end
+  def price_as_string
+    return "$"+'%.2f' % @price.to_s
+    #return "$"+@price.to_s
+  end
 end
